582ea0d0d75e9076de2d53e38e00143f4d7a87f1
Nikita <mikhaylovnikita05@gmail.com>
2025-09-06 00:25:09 +0300

    Fix SonarCloud C++ plugin error 2

---
+++ lab_1/Matrix.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class TestClass {
+public:
+    void hello() {
+        std::cout << "Hello SonarCloud!" << std::endl;
+    }
+};
+
+int main() {
+    TestClass test;
+    test.hello();
+    return 0;
+}

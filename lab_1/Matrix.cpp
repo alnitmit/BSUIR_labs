60cba44f3e084800923a34292e4fd204eb2f1973
Nikita <mikhaylovnikita05@gmail.com>
2025-09-06 18:14:27 +0300

    ggt

--- lab_1/Matrix.cpp
+++ lab_1/Matrix.cpp
@@ -1,14 +1,2 @@
 #include <iostream>
 
-class TestClass {
-public:
-    void hello() {
-        std::cout << "Hello SonarCloud!" << std::endl;
-    }
-};
-
-int main() {
-    TestClass test;
-    test.hello();
-    return 0;
-} 

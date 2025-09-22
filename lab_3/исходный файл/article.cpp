a6b265f30b559f71e27190e00a3528ff39fceaa5
Nikita <mikhaylovnikita05@gmail.com>
2025-09-22 19:00:56 +0300

    fix

--- lab_3/исходный файл/article.cpp
+++
@@ -1,7 +0,0 @@
-#include "../заголовочный файл/Article.h"
-
-Article::Article(const std::string& t, const std::string& a) 
-    : title(t), author(a) {}
-
-std::string Article::getTitle() const { return title; }
-std::string Article::getAuthor() const { return author; }

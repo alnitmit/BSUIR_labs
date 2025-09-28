474049aaa03527a948d6b02f64606a4d9de30562
Nikita <mikhaylovnikita05@gmail.com>
2025-09-29 01:20:08 +0300

    fix lab 3

--- lab_3/src/BookCard.cpp
+++ lab_3/src/BookCard.cpp
@@ -1,28 +1,33 @@
 #include "../include/BookCard.h"
 
-BookCard::BookCard(const std::string &a, const std::string &t,
-                   const std::string &am, const std::string &in,
-                   const std::string &tcc, const std::string &p, int y, int c,
-                   int pg)
+BookCard::BookCard(const std::string& a, const std::string& t,
+                   const std::string& am, const std::string& in,
+                   const std::string& tcc, const std::string& p,
+                   int y, int c, int pg)
     : IndependentPublishingCard(a, t, am, in, tcc, p, y, c, pg),
       article(new Article(t, a)) {}
 
-BookCard::BookCard(BookCard &&other) noexcept
-    : IndependentPublishingCard(std::move(other)), article(other.article) {
-  other.article = nullptr;
-}
-
-BookCard &BookCard::operator=(BookCard &&other) noexcept {
-  if (this != &other) {
-    IndependentPublishingCard::operator=(std::move(other));
-
-    delete article;
-    article = other.article;
+BookCard::BookCard(BookCard&& other) noexcept
+    : IndependentPublishingCard(std::move(other)),
+      article(other.article) {
     other.article = nullptr;
-  }
-  return *this;
 }
 
-BookCard::~BookCard() { delete article; }
+BookCard& BookCard::operator=(BookCard&& other) noexcept {
+    if (this != &other) {
+        IndependentPublishingCard::operator=(std::move(other));
+        
+        delete article;
+        article = other.article;
+        other.article = nullptr;
+    }
+    return *this;
+}
 
-Article *BookCard::getArticle() const { return article; }
+BookCard::~BookCard() {
+    delete article;
+}
+
+Article* BookCard::getArticle() const { 
+    return article; 
+}

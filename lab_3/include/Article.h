57fd70c982133705c478dc94d3f5a4eec823e656
Nikita <mikhaylovnikita05@gmail.com>
2025-09-30 09:46:50 +0300

    fix lab 3

--- lab_3/include/Article.h
+++ lab_3/include/Article.h
@@ -7,11 +7,32 @@ private:
     std::string author;
 
 public:
+    // Конструктор
     Article(const std::string& t, const std::string& a);
+    
+    // Правило пяти: деструктор, копирование, перемещение
     ~Article();
-
+    
+    // Конструктор копирования
+    Article(const Article& other);
+    
+    // Оператор присваивания копированием
+    Article& operator=(const Article& other);
+    
+    // Конструктор перемещения
+    Article(Article&& other) noexcept;
+    
+    // Оператор присваивания перемещением
+    Article& operator=(Article&& other) noexcept;
+    
+    // Геттеры
     std::string getTitle() const;
     std::string getAuthor() const;
+    
+    // Сеттеры
     void setTitle(const std::string& t);
     void setAuthor(const std::string& a);
+    
+    // Метод обмена
+    void swap(Article& other) noexcept;
 };
--- lab_3/src/Article.cpp
+++ lab_3/src/Article.cpp
@@ -1,13 +1,64 @@
 #include "../include/Article.h"
+#include <utility> // для std::swap
 
+// Конструктор
 Article::Article(const std::string& t, const std::string& a)
     : title(t), author(a) {}
 
+// Деструктор
 Article::~Article() {
     title.clear();
     author.clear();
 }
 
+// Конструктор копирования
+Article::Article(const Article& other)
+    : title(other.title), author(other.author) {}
+
+// Оператор присваивания копированием
+Article& Article::operator=(const Article& other) {
+    if (this != &other) {
+        // Используем идиому copy-and-swap
+        Article temp(other);
+        swap(temp);
+    }
+    return *this;
+}
+
+// Конструктор перемещения
+Article::Article(Article&& other) noexcept
+    : title(std::move(other.title)), author(std::move(other.author)) {
+    // Приводим moved-from объект в валидное состояние
+    other.title.clear();
+    other.author.clear();
+}
+
+// Оператор присваивания перемещением
+Article& Article::operator=(Article&& other) noexcept {
+    if (this != &other) {
+        // Освобождаем текущие ресурсы
+        title.clear();
+        author.clear();
+        
+        // Перемещаем ресурсы из other
+        title = std::move(other.title);
+        author = std::move(other.author);
+        
+        // Приводим other в валидное состояние
+        other.title.clear();
+        other.author.clear();
+    }
+    return *this;
+}
+
+// Метод обмена
+void Article::swap(Article& other) noexcept {
+    using std::swap;
+    swap(title, other.title);
+    swap(author, other.author);
+}
+
+// Геттеры
 std::string Article::getTitle() const { 
     return title; 
 }
@@ -16,6 +67,7 @@ std::string Article::getAuthor() const {
     return author; 
 }
 
+// Сеттеры
 void Article::setTitle(const std::string& t) { 
     title = t; 
 }

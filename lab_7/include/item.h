26e5d0781c0fd0bd7db1383dd2859f8920cae49c
Nikita <mikhaylovnikita05@gmail.com>
2025-10-20 20:20:27 +0300

    fix lab 7

--- lab_7/include/item.h
+++ lab_7/include/item.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <cstddef>
 
 struct Item {
     int id;
@@ -24,5 +25,7 @@ bool getValidInput(const std::string& prompt, int& value);
 bool getValidInput(const std::string& prompt, double& value);
 bool findItem(int id, Item& foundItem, long& position);
 bool updateItemInFile(long position, const Item& item);
+void writeItem(std::ostream& os, const Item& item);
+bool readItem(std::istream& is, Item& item);
 
 #endif
--- lab_7/src/inventory.cpp
+++ lab_7/src/inventory.cpp
@@ -5,6 +5,46 @@
 const std::string FILENAME = "inventory.dat";
 const int NAME_SIZE = 50;
 
+template<typename T>
+void writeAsBytes(std::ostream& os, const T& obj) {
+    os.write(reinterpret_cast<const char*>(&obj), sizeof(obj));
+}
+
+template<typename T>
+void readAsBytes(std::istream& is, T& obj) {
+    is.read(reinterpret_cast<char*>(&obj), sizeof(obj));
+}
+
+void writeString(std::ostream& os, const std::string& str) {
+    size_t size = str.size();
+    writeAsBytes(os, size);
+    os.write(str.c_str(), static_cast<std::streamsize>(size));
+}
+
+void readString(std::istream& is, std::string& str) {
+    size_t size;
+    readAsBytes(is, size);
+    str.resize(size);
+    is.read(&str[0], static_cast<std::streamsize>(size));
+}
+
+void writeItem(std::ostream& os, const Item& item) {
+    writeAsBytes(os, item.id);
+    writeString(os, item.name);
+    writeAsBytes(os, item.quantity);
+    writeAsBytes(os, item.cost);
+    writeAsBytes(os, item.active);
+}
+
+bool readItem(std::istream& is, Item& item) {
+    readAsBytes(is, item.id);
+    readString(is, item.name);
+    readAsBytes(is, item.quantity);
+    readAsBytes(is, item.cost);
+    readAsBytes(is, item.active);
+    return !is.fail();
+}
+
 bool getValidInput(const std::string& prompt, int& value) {
     if (!prompt.empty()) {
         std::cout << prompt;
@@ -44,7 +84,7 @@ void addItem() {
 
     if (std::ifstream inFile(FILENAME, std::ios::binary); inFile) {
         Item temp{};
-        while (inFile.read(reinterpret_cast<char*>(&temp), sizeof(Item))) {
+        while (readItem(inFile, temp)) {
             if (temp.active && temp.id == newItem.id) {
                 std::cout << "Item with this ID already exists.\n";
                 return;
@@ -62,7 +102,7 @@ void addItem() {
     newItem.active = true;
 
     if (std::ofstream outFile(FILENAME, std::ios::binary | std::ios::app); outFile) {
-        outFile.write(reinterpret_cast<const char*>(&newItem), sizeof(Item));
+        writeItem(outFile, newItem);
         std::cout << "Item added successfully.\n";
     } else {
         std::cout << "Error opening file for writing.\n";
@@ -78,7 +118,7 @@ void displayAllItems() {
     std::ifstream file(FILENAME, std::ios::binary);
     Item temp{};
     bool found = false;
-    while (file.read(reinterpret_cast<char*>(&temp), sizeof(Item))) {
+    while (readItem(file, temp)) {
         if (temp.active) {
             std::cout << "ID: " << temp.id << ", Name: " << temp.name
                       << ", Quantity: " << temp.quantity << ", Cost: " << temp.cost << '\n';
@@ -99,12 +139,12 @@ bool findItem(int id, Item& foundItem, long& position) {
     std::ifstream file(FILENAME, std::ios::binary);
     position = 0;
     Item temp{};
-    while (file.read(reinterpret_cast<char*>(&temp), sizeof(Item))) {
+    while (readItem(file, temp)) {
         if (temp.active && temp.id == id) {
             foundItem = temp;
             return true;
         }
-        position += sizeof(Item);
+        position = file.tellg();
     }
     return false;
 }
@@ -116,8 +156,8 @@ bool updateItemInFile(long position, const Item& item) {
 
     std::fstream file(FILENAME, std::ios::binary | std::ios::in | std::ios::out);
     file.seekp(position);
-    file.write(reinterpret_cast<const char*>(&item), sizeof(Item));
-    return true;
+    writeItem(file, item);
+    return !file.fail();
 }
 
 void deleteItem() {
